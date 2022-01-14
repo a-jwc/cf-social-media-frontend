748a35216acc3ff8df7b8d5f60f82b928c2d47c7
a-jwc <chauajw@gmail.com>
2022-01-14 08:47:09 -0800

    update font, stlying, and node

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -31,21 +31,21 @@ const Posts = (props: RouteComponentProps) => {
 
 	console.log(postsJson);
 	return (
-		<div className="h-full w-auto mx-auto grid grid-cols-1 grid-rows-1 grid-flow-col font-sans bg-primary-500">
-			<div className="flex flex-col grow">
-				<header className="text-center text-3xl text-white p-4">
+		<div className="h-full w-auto mx-auto grid grid-cols-1 grid-rows-1 grid-flow-col font-sans bg-primary-500 font-all">
+			<div className="flex flex-col grow mb-32">
+				<header className="text-center text-3xl text-white p-4 tracking-wide">
 					<h1>Posts</h1>
 				</header>
 				{postsJson.map((post: PostProps) => (
 					<section
 						key={post.title}
-						className="flex flex-col items-start bg-blend-soft-light bg-slate-100 border-2 border-grey-600 hover:border-indigo-400 hover:bg-indigo-300 hover:scale-105 p-4 m-4 md:w-1/3 mx-auto col-start-1 col-end-2 rounded-md"
+						className="flex flex-col items-start bg-blend-soft-light bg-slate-100 border-2 border-grey-600 hover:border-indigo-400 hover:bg-indigo-200 hover:scale-105 p-4 m-4 md:w-1/3 mx-auto col-start-1 col-end-2 rounded-md"
 					>
 						<h1 className="basis-1/3 w-auto flex-shrink pr-4 flex-grow-0 m-1 leading-tight">
 							<Link to={`/posts/${post.title}`}>
 								<p className="font-medium">{post.title}</p>
 							</Link>
-							<Link to={`/user`}>
+							<Link to={`/user/${post.username}`}>
 								<small className=" text-gray-500">{post.username}</small>
 							</Link>
 						</h1>
