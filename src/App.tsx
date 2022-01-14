e6deb5873b68ce8ea7c82168b149a9c75728b7c1
a-jwc <chauajw@gmail.com>
2022-01-14 09:18:11 -0800

    update components path

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Router, RouteComponentProps } from "@reach/router";
 import "./styles/globals.css";
 
-import Posts from "../../social-media-frontend/src/components/posts";
-import Post from "../../social-media-frontend/src/components/post";
+import Posts from "./components/posts";
+import Post from "./components/post";
 import { Sidebar } from "./components/sidebar";
 
 function App() {
