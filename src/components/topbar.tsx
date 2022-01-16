821b1d9f8c13c670c1be201539a6e6adc73d0d09
a-jwc <chauajw@gmail.com>
2022-01-15 16:07:04 -0800

    update styling

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -4,7 +4,7 @@ export const TopBar = () => {
 	return (
 		<nav className="bg-gray-900 ml-12">
 			<div className="w-full mx-auto px-2 sm:px-6 lg:px-8">
-				<div className="relative flex items-center justify-end h-16">
+				<div className="relative flex items-center justify-end h-14">
 					<a href="https://www.alextheproxy.com/" target="_blank" rel="noreferrer">
 						<strong className="text-white w-auto tracking-widest">AJWC</strong>
 					</a>
