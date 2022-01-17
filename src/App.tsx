1069e5d26368f6a8f75516133799d0665f9b5eeb
a-jwc <chauajw@gmail.com>
2022-01-17 04:18:42 -0800

    move topbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet";
 import Posts from "./components/posts";
 import Post from "./components/post";
 import { Sidebar } from "./components/sidebar";
+import { TopBar } from "./components/topbar";
 
 function App() {
 	return (
@@ -14,6 +15,8 @@ function App() {
 				<meta name="theme-color" content="#45507B" />
 			</Helmet>
 			<Sidebar />
+      <TopBar />
+
 			<Router>
 				<Posts path="/" />
 				<Post path="/posts/:id" title={""} content={""} />
