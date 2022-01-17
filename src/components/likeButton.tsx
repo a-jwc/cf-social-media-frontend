105bd9134f4c4550cd494c834a2985c3eb6d08e0
a-jwc <chauajw@gmail.com>
2022-01-17 06:00:42 -0800

    add like button interface and fetch

--- src/components/likeButton.tsx
+++ src/components/likeButton.tsx
@@ -1,15 +1,40 @@
 import { AiOutlineLike, AiFillLike } from "react-icons/ai";
 import React, { useState } from "react";
 
-interface Likes {
+interface PostProps {
+	title: string;
+	username: string;
+	content: string;
+	time: string;
+	imageUrl: string;
 	votes: number;
 }
 
-export const LikeButton = (props: Likes) => {
+export const LikeButton = (props: PostProps) => {
 	const [votes, setVotes] = useState(props.votes);
 
-	const handleClick = () => {
+	let body = {
+		title: props.title,
+		username: props.username,
+		content: props.content,
+		time: props.time,
+		imageUrl: props.imageUrl,
+		votes: props.votes + 1,
+	};
+	console.log("body", body);
+	const handleClick = async () => {
 		setVotes(votes + 1);
+		const resp = await fetch(
+			"https://workers-rust.chauajw.workers.dev/updatelikes",
+			{
+				mode: "cors",
+				method: "POST",
+				body: JSON.stringify(body),
+			}
+		);
+
+		const postsResp = await resp.json();
+		console.log("resp", postsResp);
 	};
 
 	return (
