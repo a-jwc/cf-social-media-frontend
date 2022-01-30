7690943a668cd2ae03979c065b93334bf5ea671f
a-jwc <chauajw@gmail.com>
2022-01-30 01:06:25 -0800

    refactor Post component

---
+++ src/components/post.tsx
@@ -0,0 +1,26 @@
+import { Link } from "@reach/router";
+import { VoteButton } from "./voteButton";
+import { PostProps } from "../ts/interfaces";
+
+export const Post = (post: PostProps) => {
+	return (
+		<section
+			key={post.title}
+			className="flex flex-col items-start bg-slate-100 border-2 border-grey-600 p-4 m-4 w-1/2 mx-auto col-start-1 col-end-2 rounded-md"
+		>
+			<h1 className="basis-1/3 w-auto pr-4 flex-grow-0 m-1 leading-tight">
+				<p className="font-medium">{post.title}</p>
+				<Link to={`/user/${post.username}`}>
+					<small className=" text-gray-500">{post.username}</small>
+				</Link>
+			</h1>
+			<div className="basis-2/3 flex-grow m-1">
+				<p>{post.content}</p>
+			</div>
+			{post.imageUrl.length > 0 ? (
+				<img src={post.imageUrl} alt="replacement" />
+			) : null}
+			<VoteButton {...post} />
+		</section>
+	);
+};
--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,40 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "@reach/router";
 import { RouteComponentProps } from "@reach/router";
 import { CreatePost } from "./createPost";
-import { VoteButton } from "./voteButton";
-import { PostProps } from "../common/types";
-// interface PostProps {
-// 	title: string;
-// 	username: string;
-// 	content: string;
-// 	time: string;
-// 	imageUrl: string;
-// 	votes: number;
-// }
-
-const Post = (post: PostProps) => {
-	return (
-		<section
-			key={post.title}
-			className="flex flex-col items-start bg-slate-100 border-2 border-grey-600 p-4 m-4 w-1/2 mx-auto col-start-1 col-end-2 rounded-md"
-		>
-			<h1 className="basis-1/3 w-auto pr-4 flex-grow-0 m-1 leading-tight">
-				<p className="font-medium">{post.title}</p>
-				<Link to={`/user/${post.username}`}>
-					<small className=" text-gray-500">{post.username}</small>
-				</Link>
-			</h1>
-			<div className="basis-2/3 flex-grow m-1">
-				<p>{post.content}</p>
-			</div>
-			{post.imageUrl.length > 0 ? (
-				<img src={post.imageUrl} alt="replacement" />
-			) : null}
-			<VoteButton {...post} />
-		</section>
-	);
-};
+import { PostProps } from "../ts/interfaces";
+import { Post } from "./post";
 
 const Posts = (props: RouteComponentProps) => {
 	const [posts, setPosts] = useState<PostProps[]>([]);
