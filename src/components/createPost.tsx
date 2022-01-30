f924049766e9ddfd2ce02b0b446a0eee6f469b8d
a-jwc <chauajw@gmail.com>
2022-01-29 16:27:28 -0800

    clean up

--- src/components/createPost.tsx
+++ src/components/createPost.tsx
@@ -41,18 +41,8 @@ export const CreatePost: React.FunctionComponent = () => {
 			setImageUrl("");
 
 			submitPost(body);
+      getPosts();
 		}
-
-		// const users = await fetch(
-		// 	"https://workers-rust.chauajw.workers.dev/users",
-		// 	{
-		// 		mode: "cors",
-		// 		method: "GET",
-		// 	}
-		// );
-
-		// const usersResp: any[] = await users.json();
-		// console.log("users", usersResp);
 	};
 
 	const handleFileUpload = async (e: any) => {
@@ -115,95 +105,3 @@ export const CreatePost: React.FunctionComponent = () => {
 		</div>
 	);
 };
-
-// * ES6 CreatePost
-// interface MyProps {}
-
-// interface FormState {
-// 	username: string;
-// 	title: string;
-// 	content: string;
-// }
-
-// class CreatePost extends React.Component<MyProps, FormState> {
-// 	constructor(props: MyProps | Readonly<MyProps>) {
-// 		super(props);
-// 		this.state = {
-// 			username: "",
-// 			title: "",
-// 			content: "",
-// 		};
-// 		this.handleSubmit = this.handleSubmit.bind(this);
-// 	}
-
-// 	async handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-// 		event.preventDefault();
-// 		let now = new Date().toISOString();
-// 		let body = {
-// 			content: this.state.content,
-// 			time: now,
-// 			username: this.state.username,
-// 			title: this.state.title,
-// 		};
-// 		console.log(body);
-// 		this.setState({
-// 			username: "",
-// 			title: "",
-// 			content: "",
-// 		});
-// 		const resp = await fetch("https://workers-rust.chauajw.workers.dev/posts", {
-// 			mode: "cors",
-// 			method: "POST",
-// 			body: JSON.stringify(body),
-// 		});
-// 		const postsResp = await resp.json();
-// 		console.log(postsResp);
-// 	}
-
-// 	render() {
-// 		return (
-// 			<div className="flex flex-row w-3/4 mx-auto text-white content-center border-2 p-4 border-primary-600 rounded-md">
-// 				<form onSubmit={this.handleSubmit}>
-// 					<header className="text-center text-3xl text-white tracking-wide">
-// 						<h1>Make a Post</h1>
-// 					</header>
-// 					<fieldset>
-// 						<label className="mb-1 mt-2">Username</label>
-// 						<input
-// 							type="text"
-// 							value={this.state.username}
-// 							onChange={(e) => this.setState({ username: e.target.value })}
-// 							placeholder="Username"
-// 							className="text-black mb-2 w-full rounded p-1"
-// 						/>
-// 						<label className="mb-1 mt-2">Title</label>
-// 						<input
-// 							type="text"
-// 							value={this.state.title}
-// 							onChange={(e) => this.setState({ title: e.target.value })}
-// 							placeholder="Title"
-// 							className="text-black mb-2 w-full rounded p-1"
-// 						/>
-// 						<label className="mb-1 mt-2">Post</label>
-// 						<textarea
-// 							// type="textarea"
-// 							value={this.state.content}
-// 							rows={4}
-// 							onChange={(e) => this.setState({ content: e.target.value })}
-// 							placeholder="Post"
-// 							className="text-black mb-2 w-full rounded p-1"
-// 						/>
-// 					</fieldset>
-// 					<button
-// 						type="submit"
-// 						className="bg-indigo-300 mt-2 p-2 rounded-md m-auto w-full hover:bg-gradient-to-b from-primary-700 via-primary-800 to-primary-600"
-// 					>
-// 						Submit
-// 					</button>
-// 				</form>
-// 			</div>
-// 		);
-// 	}
-// }
-
-// export { CreatePost };
