0ed1804dd14551b1946dc216d7d7540d78a5e3d1
a-jwc <chauajw@gmail.com>
2022-01-31 17:31:53 -0800

    update color theme

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -9,7 +9,7 @@ const Posts = (props: RouteComponentProps) => {
 	const postsMap = useMapPosts(posts);
 
 	return (
-		<div className="h-full w-full mx-auto grid grid-cols-1 grid-rows-1 grid-flow-col font-sans bg-primary-500 font-all">
+		<div className="h-full w-full mx-auto grid grid-cols-1 grid-rows-1 grid-flow-col font-sans bg-primary-800 font-all">
 			<div className="flex flex-col mb-32">
 				<div className="mt-10 mb-8 mx-auto">
 					<CreatePost setPosts={setPosts} posts={posts} />
--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "@reach/router";
 
 export const TopBar = () => {
 	return (
-		<nav className="bg-gray-900 ml-12">
+		<nav className="bg-primary-800 ml-12">
 			<div className="w-full mx-auto px-2 sm:px-6 lg:px-8">
 				<div className="relative flex items-center justify-end h-14">
 					<a href="https://www.alextheproxy.com/" target="_blank" rel="noreferrer">
